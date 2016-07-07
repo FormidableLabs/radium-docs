5520334f925d62fdfb5cbf838ad6353b854d2ff4
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-07-07 11:32:47 -0700

    add route for docs

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,11 @@ import { Route, IndexRoute } from "react-router";
 // Components
 import App from "./components/app";
 import Home from "./views/home";
-//import Docs from "./views/docs";
+import Docs from "./views/docs";
 
 module.exports = (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
+    <Route path="/docs" component={Docs} />
   </Route>
 );
---
+++ src/views/docs/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Docs extends React.Component {
+  render() {
+    return <div>docs</div>;
+  }
+}
+
+export default Docs;
