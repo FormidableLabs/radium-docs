67b73c26e39340c9da6b012925e0fbe0b437cb61
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-07-12 16:17:13 -0700

    wip figure out routing woes

--- src/components/entry.jsx
+++ src/components/entry.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
-import { Router, browserHistory, applyRouterMiddleware } from "react-router";
+import { Router, useRouterHistory, applyRouterMiddleware } from "react-router";
 import useScroll from "react-router-scroll";
 import routes from "../routes";
+import { useBasename } from "history";
+import createBrowserHistory from "history/lib/createBrowserHistory";
+
+const history = useRouterHistory(useBasename(createBrowserHistory))({
+  basename: "/"
+});
 
 render(
   <Router
-    history={browserHistory}
+    history={history}
     routes={routes}
     render={applyRouterMiddleware(useScroll())}
   />,
