37d5cef80b67c395a056d3709bd03b90d04fc754
Kyle Cesmat <kyle.cesmat@formidable.com>
2016-07-28 14:10:33 -0700

    fix ordering of styles, hardcode user-agent

--- src/components/app.js
+++ src/components/app.js
@@ -11,10 +11,14 @@ class App extends React.Component {
   }
 
   render() {
+    const isBrowser = typeof window !== "undefined" && window.__STATIC_GENERATOR !== true;
     return (
-      <StyleRoot style={{display: "flex", flexDirection: "column", height: "100%"}}>
-        {this.props.children}
+      <StyleRoot
+        radiumConfig={isBrowser ? { userAgent: window.navigator.userAgent } : null}
+        style={{display: "flex", flexDirection: "column", height: "100%"}}
+      >
         <Style rules={stylesheet} />
+        {this.props.children}
       </StyleRoot>
     );
   }
